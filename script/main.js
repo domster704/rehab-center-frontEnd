cb3aa21c2d00e3639e6ee2b838fe4247e1fc6f0a
domster704 <domster704@mail.ru>
2023-08-02 02:36:24 +0300

    fix some problems

--- script/main.js
+++ script/main.js
@@ -13,3 +13,38 @@ window.onresize = () => {
     VW_IN_PX = window.innerWidth / 100.0;
     nextPhotoInSlider(document.querySelector('.main-6-block-slider-range'));
 }
+
+let isImageSliderDrag = false;
+let posInit = 0;
+let posX1 = 0;
+let posX2 = 0;
+let style;
+let transform;
+let trfRegExp = /[-0-9.]+(?=px)/;
+const swipeStart = function (event) {
+    posInit = posX1 = event.clientX;
+    imageSlider.addEventListener('mousemove', swipeAction);
+    imageSlider.addEventListener('touchmove', swipeAction);
+    imageSlider.addEventListener('touchend', swipeEnd);
+    imageSlider.addEventListener('mouseup', swipeEnd);
+}
+
+const swipeAction = function (event) {
+    style = imageSlider.style.transform;
+    console.log(style)
+    transform = +style.match(trfRegExp)[0];
+
+    posX2 = posX1 - event.clientX;
+    posX1 = event.clientX;
+
+    imageSlider.style.transform = `translate3d(${transform - posX2}px, 0px, 0px)`;
+}
+
+const swipeEnd = function () {
+    imageSlider.removeEventListener('touchmove', swipeAction);
+    imageSlider.removeEventListener('mousemove', swipeAction);
+    imageSlider.removeEventListener('touchend', swipeEnd);
+    imageSlider.removeEventListener('mouseup', swipeEnd);
+}
+
+imageSlider.addEventListener('mousedown', swipeStart);
